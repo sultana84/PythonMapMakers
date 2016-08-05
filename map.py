a4759a419a63d3dee7f55db9f4e7c61cf80841bd
sultana84 <sultanaisgneiss@gmail.com>
2016-08-05 00:54:13 +0000

    adding survey

--- map.py
+++ map.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/home')
 def home():
     return render_template("home.html")
+def form():
+    return render_template("form.html")
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, host='0.0.0.0')
