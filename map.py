bcab021bf9aaf62078361675957144e8a7afdfbd
sultana84 <sultanaisgneiss@gmail.com>
2016-08-08 23:15:15 +0000

    updating files

--- map.py
+++ map.py
@@ -6,9 +6,15 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route('/form')
-def form():
-    return render_template("form.html")
+@app.route('/submitsurvey', methods=['POST'])
+def submitsurvey():
+ price = request.form.get("price")
+ rating = request.form.get("rating")
+ latitude = float(request.form.get("latitude"))
+ longitude = float(request.form.get("longitude"))
+ comments = request.form.get("comments")
+ DB.restaurantmap(price, rating, latitude, longitude, comments)
+ return home()
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, host='0.0.0.0')
