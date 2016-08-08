aa7f2f9188ab9a12cc1cf739fa6ec2e1d5d1ffb4
Santiago0076 <santiago007roman@hotmail.com>
2016-08-08 23:15:06 +0000

    SR: Login files

--- dbhelp.py
+++ dbhelp.py
@@ -1,9 +1,13 @@
-MOCK_USERS = {'test@gmail.com': '654321'}
+MOCK_USERS = [{"email": "test@gmail.com", "salt": "e10adc3949ba59abbe56e057f20f883e=", "hashed": "123456"}]
 
 class dbhelp:
 
     def get_user(self,email):
-        if email in MOCK_USERS:
-            return MOCK_USERS[email]
+        user = [x for x in MOCK_USERS if x.get("email") == email]
+        if user:
+            return user[email]
         return None
+
+    def add_user(self, email, salt, hashed):
+     MOCK_USERS.append({"email": email, "salt": salt, "hashed": hashed})
 
